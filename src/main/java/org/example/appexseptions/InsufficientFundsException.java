754a53007f1dcd4f49ad209c397e106445c7e82e
Артём <artempolevoj@yandex.ru>
2023-11-28 15:02:17 +0300

    add InsufficientFundsException

---
+++ src/main/java/org/example/appexseptions/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package org.example.appexseptions;
+
+public class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
